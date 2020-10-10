05a67553b7c778b0f8d6a6878fd10db4f5cca703
Sébastien GELDREICH <trehinos@gmail.com>
2020-10-10 03:05:52 +0200

    Reorganised application.php

--- application.php
+++ application.php
@@ -13,79 +13,25 @@
 
 require_once __DIR__ . '/vendors/autoload.php';
 
-use Symfony\Component\Yaml\Yaml;
-
-use Thor\Cli\CliKernel;
+use Thor\Globals;
+use Thor\Application;
+use Thor\Debug\Logger;
 use Thor\ConfigurationLoader;
 use Thor\Database\DefinitionHelper;
 use Thor\Database\PdoExtension\AdvancedPdoRow;
-use Thor\Debug\Logger;
-use Thor\Globals;
-use Thor\Http\HttpKernel;
-use Thor\Application;
 
-// Create logger and load base configuration
-Logger::getDefaultLogger('prod', Globals::CODE_DIR);
-['config' => $config, 'database' => $databases] = ConfigurationLoader::loadConfig('config', 'database');
-$thor_env = $config['env'] ?? 'debug';
-$path = $config['log_path'] ?? 'var/';
-Logger::getDefaultLogger($thor_env, Globals::CODE_DIR . $path);
-$lang = $config['lang'] ?? 'fr';
-$language = Yaml::parseFile(Globals::STATIC_DIR . "langs/$lang.yml");
-ini_set('date.timezone', $config['timezone'] ?? 'Europe/Paris');
+// Load configuration
+Application::loadMainConfiguration();
+Application::setErrorReporting();
 
-// Set error reporting level
-if ('prod' === $thor_env) {
-    ini_set('display_errors', 0);
-} elseif ('debug' === $thor_env) {
-    ini_set('display_errors', E_ERROR | E_WARNING | E_PARSE);
-} else {
-    ini_set('display_errors', E_ALL);
-}
-
-// Static state
+// Set global static state
+$path = Application::$config['log_path'] ?? 'var/';
+Logger::getDefaultLogger(Application::$thor_env, Globals::CODE_DIR . $path);
 AdvancedPdoRow::$definitionHelper = new DefinitionHelper(
     ConfigurationLoader::loadStatic('db_definition')['db_definition']['schema'] ?? []
 );
 
-
-// Execute kernel
-$kernel = null;
-$sapi = php_sapi_name();
-try {
-    switch ($thor_kernel ?? null) {
-        case 'http':
-            $kernel = new HttpKernel(
-                ['databases' => $databases, 'language' => $language] +
-                ConfigurationLoader::loadConfig('twig') +
-                ConfigurationLoader::loadStatic('routes')
-            );
-            break;
-
-        case 'cli':
-            $kernel = CliKernel::createCli(ConfigurationLoader::loadStatic('commands'));
-            break;
-
-        case 'repl':
-        case 'automaton':
-            echo "Error :\nNot implemented $thor_kernel kernel.\n";
-            break;
-    }
-
-    $app = new Application($kernel);
-    Logger::write('Execute application');
-    $app->execute();
-    Logger::write('Application executed !');
-} catch (Throwable $e) {
-    // Catches all uncatched exception to log them in log file.
-    Logger::logThrowable($e);
-    echo "UNRECOVERABLE ERROR\n";
-    if (in_array($thor_env, ['dev', 'debug'])) {
-        echo "<pre>";
-        throw $e;
-    }
-}
-
-Logger::write('END ### END', Logger::DEV);
+// Start application
+Application::executeKernel($thor_kernel ?? null);
 
 exit; // make sure this script can't be included with further actions
--- thor/Application.php
+++ thor/Application.php
@@ -2,7 +2,13 @@
 
 namespace Thor;
 
+use Exception;
+
+use Symfony\Component\Yaml\Yaml;
+use Thor\Cli\CliKernel;
 use Thor\Debug\Logger;
+use Thor\Http\HttpKernel;
+use Throwable;
 
 final class Application implements KernelInterface
 {
@@ -14,14 +20,100 @@ final class Application implements KernelInterface
         $this->kernel = $kernel;
     }
 
+    /**
+     * @throws Exception
+     */
     public function execute()
     {
-        if (null == $this->kernel) {
+        if (null === $this->kernel) {
             Logger::write("Application fatal error : kernel not defined.");
-            echo "Error :\nKernel not selected.\n";
-            exit;
+            echo "Error : Kernel not selected.\n";
+
+            throw new Exception("Application fatal error : kernel not defined.");
         }
         $this->kernel->execute();
+    }
+
+    public static string $thor_env = 'dev';
+    public static array $config = [];
+    public static array $language = [];
+    public static array $databases = [];
+
+    /**
+     * Loads $config, $databases and $language
+     */
+    public static function loadMainConfiguration(): void
+    {
+        Logger::getDefaultLogger('prod', Globals::CODE_DIR);
+        ['config' => self::$config, 'database' => self::$databases] = ConfigurationLoader::loadConfig(
+            'config',
+            'database'
+        );
+        self::$thor_env = self::$config['env'] ?? 'debug';
+        $lang = $config['lang'] ?? 'fr';
+        self::$language = Yaml::parseFile(Globals::STATIC_DIR . "langs/$lang.yml");
+        ini_set('date.timezone', $config['timezone'] ?? 'Europe/Paris');
+    }
+
+    /**
+     * $thor_env: 'dev' => E_ALL, 'debug' => E_ERROR|E_WARNING|E_PARSE, 'prod' => 0
+     */
+    public static function setErrorReporting(): void
+    {
+        if ('prod' === self::$thor_env) {
+            ini_set('display_errors', 0);
+        } elseif ('debug' === self::$thor_env) {
+            ini_set('display_errors', E_ERROR | E_WARNING | E_PARSE);
+        } else {
+            ini_set('display_errors', E_ALL);
+        }
+    }
+
+    /**
+     * executeKernel(): loads and executes a kernel
+     *
+     * @param string|null $thor_kernel
+     *
+     * @throws Throwable
+     */
+    public static function executeKernel(?string $thor_kernel): void
+    {
+        $kernel = null;
+        Logger::write('APP_START ### APP_START', Logger::DEV);
+        try {
+            switch ($thor_kernel) {
+                case 'http':
+                    $kernel = new HttpKernel(
+                        ['databases' => self::$databases, 'language' => self::$language] +
+                        ConfigurationLoader::loadConfig('twig') +
+                        ConfigurationLoader::loadStatic('routes')
+                    );
+                    break;
+
+                case 'cli':
+                    $kernel = CliKernel::createCli(ConfigurationLoader::loadStatic('commands'));
+                    break;
+
+                case 'repl':
+                case 'automaton':
+                    echo "Error : Not implemented $thor_kernel kernel.\n";
+                    break;
+            }
+
+            $app = new Application($kernel);
+            Logger::write('Execute application');
+            $app->execute();
+            Logger::write('Application executed !');
+        } catch (Throwable $e) {
+            // Catches all uncatched exception to log them in log file.
+            Logger::logThrowable($e);
+            echo "UNRECOVERABLE ERROR\n";
+            if (in_array(self::$thor_env, ['dev', 'debug'])) {
+                echo "<pre>";
+                throw $e;
+            }
+        }
+        Logger::write('APP_END ### APP_END', Logger::DEV);
     }
 
 }
