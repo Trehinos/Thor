e1e84ab7b9d4ec29402a7f593fafad2251f0ccd5
Sébastien GELDREICH <trehinos@gmail.com>
2022-04-07 01:25:14 +0200

    Global configuration interface, Twig global `_lang` fixed.

--- thor/Framework/Actions/Configuration.php
+++ thor/Framework/Actions/Configuration.php
@@ -3,6 +3,7 @@
 namespace Thor\Framework\Actions;
 
 use PDOException;
+use Thor\Globals;
 use Thor\Http\Routing\Route;
 use Thor\Http\Response\Response;
 use Thor\Web\WebController;
@@ -13,13 +14,24 @@ use Thor\Configuration\DatabasesConfiguration;
 final class Configuration extends WebController
 {
 
-    #[Route('config', '/config/general')]
+    #[Route('config-config', '/config/general')]
     public function configView(): Response
     {
         $config = ThorConfiguration::get();
 
-        return $this->twigResponse('thor/configuration/database.html.twig', [
-            'configuration' => $config
+        return $this->twigResponse('thor/configuration/config.html.twig', [
+            'configuration' => $config,
+            'info' => [
+                'app_vendor' => 'Marque, société, auteur',
+                'app_name' => 'Nom du projet',
+                'app_version' => '',
+                'app_version_name' => '',
+                'env' => 'DEV / DEBUG / PROD : affichage des erreurs et logs',
+                'lang' => 'fr / en : langue du système',
+                'timezone' => 'Fuseau horaire par défaut pour les opérations de date',
+                'log_path' => 'Relatif à <code>' . realpath(Globals::CODE_DIR) . '/</code>',
+                'thor_kernel' => 'Kernel actuel'
+            ]
         ]);
     }
 
