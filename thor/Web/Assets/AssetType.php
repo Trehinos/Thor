0533587e2cdceb5b76eabbe922497c2044ac0403
Trehinos <trehinos@gmail.com>
2022-04-27 12:01:20 +0200

    New assets types.

--- thor/Web/Assets/AssetType.php
+++ thor/Web/Assets/AssetType.php
@@ -9,12 +9,20 @@ enum AssetType
 
     case STYLESHEET;
     case JAVASCRIPT;
+    case IMAGE_PNG;
+    case IMAGE_GIF;
+    case IMAGE_BMP;
+    case IMAGE_JPEG;
 
     public function getExtension(): string
     {
         return match ($this) {
             self::STYLESHEET => 'css',
-            self::JAVASCRIPT => 'js'
+            self::JAVASCRIPT => 'js',
+            self::IMAGE_PNG => 'png',
+            self::IMAGE_GIF => 'gif',
+            self::IMAGE_BMP => 'bmp',
+            self::IMAGE_JPEG => 'jpg',
         };
     }
 
@@ -22,7 +30,11 @@ enum AssetType
     {
         return match ($this) {
             self::STYLESHEET => 'text/css',
-            self::JAVASCRIPT => 'application/javascript'
+            self::JAVASCRIPT => 'application/javascript',
+            self::IMAGE_PNG => 'image/png',
+            self::IMAGE_GIF => 'image/gif',
+            self::IMAGE_BMP => 'image/bmp',
+            self::IMAGE_JPEG => 'image/jpeg',
         };
     }
 
@@ -32,7 +44,8 @@ enum AssetType
         return array_combine(['tag', 'src', 'attrs'],
             match ($this) {
                 self::STYLESHEET => ['link', 'href', ['rel' => 'stylesheet']],
-                self::JAVASCRIPT => ['script', 'src', ['type' => 'application/javascript']]
+                self::JAVASCRIPT => ['script', 'src', ['type' => 'application/javascript']],
+                self::IMAGE_PNG, self::IMAGE_GIF, self::IMAGE_BMP, self::IMAGE_JPEG => ['img', 'src', []],
             }
         );
     }
@@ -40,8 +53,12 @@ enum AssetType
     public static function fromExtension(string $type): self
     {
         return match (strtolower($type)) {
-            'css'   => self::STYLESHEET,
-            'js'    => self::JAVASCRIPT,
+            'css' => self::STYLESHEET,
+            'js' => self::JAVASCRIPT,
+            'png' => self::IMAGE_PNG,
+            'gif' => self::IMAGE_GIF,
+            'bmp' => self::IMAGE_BMP,
+            'jpg', 'jpeg' => self::IMAGE_JPEG,
             default => throw new \InvalidArgumentException("Invalid asset type [$type]")
         };
     }
