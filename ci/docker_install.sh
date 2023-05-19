4349b6a3f5c0ff65703e4318ef79ff7d45c3c92b
Sébastien GELDREICH <trehinos@gmail.com>
2023-05-19 21:57:29 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /ci/docker_install.sh

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -16,3 +16,11 @@ chmod +x /usr/local/bin/phpunit
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install curl ftp intl json mbstring openssl pdo_mysql readline zip
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+mv composer.phar /usr/local/bin/composer
+chmod +x /usr/local/bin/composer
